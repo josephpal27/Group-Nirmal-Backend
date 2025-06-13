e50c842bb71006f5b546be9ff9ae1b51c17f360d
Joseph Pal <josephpal27@gmail.com>
2025-06-13 17:57:26 +0530

    Add canonical links to multiple HTML and PHP pages for SEO improvement

--- view-blog.php
+++ view-blog.php
@@ -19,6 +19,8 @@ while ($row = mysqli_fetch_assoc($result)) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog - Nirmal</title>
     <link rel="shortcut icon" href="assets/images/logo/fav.png" type="image/x-icon">
+    <!-- Link to Canonical -->
+    <link rel="canonical" href="https://groupnirmal.com/view-blog.php">
     <!-- Bootstrap CSS Link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
